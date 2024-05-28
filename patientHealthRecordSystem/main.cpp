f2050a7765490eee39a4675a5a316807af0bda9a
Ahmet Isleyen <136992260+isleyen@users.noreply.github.com>
2024-05-28 14:13:21 +0300

    Update main.cpp

--- patientHealthRecordSystem/main.cpp
+++ patientHealthRecordSystem/main.cpp
@@ -12,7 +12,7 @@ void main() {
 	hasta hasta1(3, 9, 2004, "Ahmet", "Isleyen", 27, 5, 2024, 1.99, 95);
 
 	
-	hasta hasta2(4, 11, 1998, "Burcu", "Aslan", 28, 5, 2024, 1.74, 70);
+	hasta hasta2(4, 11, 1998, "Burcu", "Aslan", 28, 5, 2024, 1.74, 70);  // Random isim ve degerler
 
 	hasta hasta3;
 	hasta hasta4;
